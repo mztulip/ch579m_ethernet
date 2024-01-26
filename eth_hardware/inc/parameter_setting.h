06bd4de953ac1f82fbb9fa0eb2a6d30d1c1f21c3
Mateusz <mateusz@tulip.lol>
2024-01-26 15:09:23 +0100

    ip config and variable name changed, unnecessary keywords removed

--- eth_hardware/inc/parameter_setting.h
+++ eth_hardware/inc/parameter_setting.h
@@ -50,11 +50,11 @@ extern  UINT8  g_cur_eth_comm_chan;
 #define  SET_NET_LEDX_FLASH()	 GPIOB_InverseBits(GPIO_Pin_7)
 
 
-#define  	LED_CARD_DEFAULT_PORT 				8800
-#define  	LED_CARD_DEFAULT_IP       		0xC0A801EB   //192.168.0.235      0xC0A800EB   0xEB00A8C0
-#define  	LED_CARD_DEFAULT_GW       		0xC0A80101    //192.168.0.1      C0A80001        0x0100A8C0
-#define  	LED_CARD_DEFAULT_DNS       		0xC0A80101      //C0A80001
-#define  	LED_CARD_DEFAULT_MASK       	0xFFFFFF00     //FFFFFF00 
+#define  	DEFAULT_PORT 			8800
+#define  	DEFAULT_IP       		0xC0A802EB   //192.168.2.235    
+#define  	DEFAULT_GW       		0xC0A80201    //192.168.2.1      
+#define  	DEFAULT_DNS       		0xC0A80101      
+#define  	DEFAULT_MASK       	0xFFFFFF00     //FFFFFF00  
 								
 #define  	GET_SYS_TICK()			gSysTicket
 
@@ -62,11 +62,10 @@ extern 	 UINT8  			gMyPeerMac[];
 extern   UINT32   			gSysTicket ;
 
 
-extern void  InitTimer0(void);
-extern void  InitAppCommon(void);
-extern void  NETLed_Init(void);
-extern void DebugInit(void);
-
-extern void  SetDefaultEthUartCfg(void);
+void  InitTimer0(void);
+void  InitAppCommon(void);
+void  NETLed_Init(void);
+void DebugInit(void);
+void  SetDefaultEthUartCfg(void);
 
 #endif 
